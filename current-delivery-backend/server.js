8fbd3dd4de8698db0f48ae8da2e0c09e3298cb2c
nazzyrichie13 <youngnazzy13@gmail.com>
2026-01-23 02:48:15 +0100

    modify

--- current-delivery-backend/server.js
+++ current-delivery-backend/server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
-app.get('*', (req, res) => {
+app.get('/:path(*)', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
 // Static folders for invoices and uploads
