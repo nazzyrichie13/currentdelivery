a146d9dbaaae1b9b3cd16f67834e3035f2e5117a
nazzyrichie13 <youngnazzy13@gmail.com>
2026-01-26 10:51:57 +0100

    modify

--- current-delivery-backend/routes/adminRoutes.js
+++ current-delivery-backend/routes/adminRoutes.js
@@ -1,9 +1,7 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-
-const Admin = require("../model/Admin");
-
+import express from 'express';
+import Admin from '../model/Admin.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 const router = express.Router();
 
 // Admin Signup
