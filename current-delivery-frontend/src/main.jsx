dd54d967eb891c5aecd7a2ade6a752fca91df268
nazzyrichie13 <youngnazzy13@gmail.com>
2026-02-16 10:56:04 +0100

    modify

--- current-delivery-frontend/src/main.jsx
+++ current-delivery-frontend/src/main.jsx
@@ -1,16 +1,13 @@
-
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import ScrollToTop from "./component/ScrollToTop";
+import "./i18n"; // ✅ important: initialize i18next before using useTranslation
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-
-    <BrowserRouter>
+  <BrowserRouter>
     <ScrollToTop />
-      <App />
-    </BrowserRouter>
-  
+    <App />
+  </BrowserRouter>
 );
-
