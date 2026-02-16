c2dd118a20d017d3c120474f5fa25209c04755a9
nazzyrichie13 <youngnazzy13@gmail.com>
2026-02-16 23:06:23 +0100

    modify

--- current-delivery-frontend/src/main.jsx
+++ current-delivery-frontend/src/main.jsx
@@ -1,9 +1,10 @@
+import "./i18n"
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import ScrollToTop from "./component/ScrollToTop";
-import "./i18n"; // ✅ important: initialize i18next before using useTranslation
+ // ✅ important: initialize i18next before using useTranslation
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
